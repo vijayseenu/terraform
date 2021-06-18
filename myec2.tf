b997dab3b7ec910147a075584728c99f80e426fd
Vijay <41150120+vijayseenu@users.noreply.github.com>
2021-06-18 20:50:56 +0530

    Create myec2.tf

---
+++ myec2.tf
@@ -0,0 +1,20 @@
+data "aws_ami" "app_ami" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm*"]
+  }
+}
+
+resource "aws_instance" "myec2" {
+  #ami = lookup(var.ami,var.region)
+  ami           = data.aws_ami.app_ami.id
+  instance_type = "t2.micro"
+  key_name      = "myprivate"
+}
+
+output "ec2_result" {
+  value = aws_instance.myec2
+}
