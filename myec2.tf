6693584f39d085411f8f84f3ad0f2027eb9ca2c6
Vijay <41150120+vijayseenu@users.noreply.github.com>
2021-06-19 12:20:47 +0530

    Update myec2.tf

--- myec2.tf
+++ myec2.tf
@@ -1,16 +1,6 @@
-data "aws_ami" "app_ami" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm*"]
-  }
-}
-
 resource "aws_instance" "myec2" {
   #ami = lookup(var.ami,var.region)
-  ami           = data.aws_ami.app_ami.id
+  ami           = "ami-0800fc0fa715fdcfe"
   instance_type = "t2.micro"
   key_name      = "myprivate"
 }
